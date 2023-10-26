08169277d545a31b640d5e770dd26bc9d3061c48
Sumit <sumitpimpare105@gmail.com>
2023-10-26 21:33:45 +0530

    project added

---
+++ src/data/ProjectData.js
@@ -0,0 +1,115 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/ykte-feature-graphic.png",
+    title: "Dashboard",
+    description:
+      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
+    tech_stack: ["React", "Webpack", "Module Federation", "HTML", "CSS"],
+  },
+  {
+    img: "/renr-responsive.png",
+    title: "GrowPro",
+    description:
+    "GrowPro: Empowering Agents for Customized Creations: Developed the GrowPro app, designed for agents to craft personalized posters and presentations. The app, built using Next.js and React.js, offers a seamless and interactive platform for users to create dynamic marketing materials.Effortless Content Management with Strapi: Seamlessly integrated Strapi for content management, enabling agents in the health insurance sector to effortlessly manage and update vital information for the GrowPro app.",
+    tech_stack: ["React.js", "Strapi.js"],
+    
+  },
+  {
+    img: "/mymind-responsive.png",
+    title: "Payment Portal",
+    description:
+      "An app where users can purchase online therapy sessions and chat with their therapists.",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
+    
+  }
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+  //   name: "React Native",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+  //   name: "Firebase",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+  //   name: "MongoDB",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+  //   name: "Python",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+  //   name: "Flask",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+  //   name: "C",
+  // },
+  {
+    img: "webpack.svg",
+    name: "Webpack",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+  //   name: "MySQL",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+  //   name: "Figma",
+  // },
+  {
+    img: "strapiIcon.svg",
+    name: "Strapi (CMS)",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
