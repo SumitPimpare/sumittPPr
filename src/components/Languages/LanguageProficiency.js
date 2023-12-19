6e0c4a04356023ef05fa940b0ad0fcdf287a50c1
sumitnathaji.pimpare <sumitnathaji.pimpare@starhealth.in>
2023-12-19 22:47:45 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/Languages/LanguageProficiency.js
+++ src/components/Languages/LanguageProficiency.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import './LanguageProficiency.css';
+import React from "react";
+import "./LanguageProficiency.css";
 const LanguageProficiency = () => {
-    const languageProficiency = [
-        { language: 'English', level: 'Professional' },
-        { language: 'Japanese', level: 'Elementary' },
-        { language: 'Hindi', level: 'professional' },
-        { language: 'Marathi', level: 'Native' },
-    ];
-    return (
-        <div className="LanguageCard">
-            <div className='lagProTitle'>Language Proficiency</div>
-            <ul className="language-list">
-                {languageProficiency.map((item, index) => (
-                    <li key={index} className="language-item">
-                        <span className="language-name">{item.language}:</span>
-                        <div className="alignLevel"><span className="language-level">{item.level}</span></div> 
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const languageProficiency = [
+    { language: "English", level: "Professional" },
+    { language: "Japanese", level: "Elementary" },
+    { language: "Hindi", level: "Professional" },
+    { language: "Marathi", level: "Native" },
+  ];
+  return (
+    <div className="LanguageCard">
+      <div className="lagProTitle">Language Proficiency</div>
+      <ul className="language-list">
+        {languageProficiency.map((item, index) => (
+          <li key={index} className="language-item">
+            <span className="language-name">{item.language}:</span>
+            <div className="alignLevel">
+              <span className="language-level">{item.level}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default LanguageProficiency;
